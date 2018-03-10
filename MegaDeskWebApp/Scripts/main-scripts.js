c495c969d12923a4b9f7cd55fa1fde82551dcda6
moakem <moa13007@byui.edu>
2018-03-10 12:47:37 -0700

    Visual Clues for Desk Material Select made

---
+++ MegaDeskWebApp/Scripts/main-scripts.js
@@ -0,0 +1,22 @@
+﻿function selectWoodImage(selectElement) {
+    var selected = selectElement.selectedIndex;
+    switch (selected) {
+        case 0:
+            document.getElementById('woodImage').style.backgroundImage = "url(/Images/pine.jpg)";
+            break;
+        case 1:
+            document.getElementById('woodImage').style.backgroundImage = "url(/Images/laminate.jpg)";
+            break;
+        case 2:
+            document.getElementById('woodImage').style.backgroundImage = "url(/Images/oak.jpg)";
+            break;
+        case 3:
+            document.getElementById('woodImage').style.backgroundImage = "url(/Images/rosewood.jpg)";
+            break;
+        case 4:
+            document.getElementById('woodImage').style.backgroundImage = "url(/Images/veneer.jpg)";
+            break;
+        default:
+            document.getElementById('woodImage').style.backgroundImage = "none";
+    }
+}
